ce8a25f5eaf915e6d607311cbff4b3a7cdec70ea
Jamesk1281 <26jkouvlis@xbhs.net>
2024-09-23 19:52:49 -0400

    Initial Commit

---
+++ ChessML/ChessMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessMLApp.swift
+//  ChessML
+//
+//  Created by James . on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ChessMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
